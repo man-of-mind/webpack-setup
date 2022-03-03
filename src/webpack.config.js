2580b0751072ce76397cdce491bc3ebb8859fc15
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-03-03 23:40:22 +0100

    webpack harrddd

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -5,6 +5,22 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "./"
+    },
+    module: {
+        rules: [{
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: "asset/resource"
+            }
+        ]
     }
 }
